659d101373aa6436834151c8263bb01900c2be0e
muhriddinbek-off <muhriddinusanov7@gmail.com>
2023-04-01 12:08:43 +0500

    first_project

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     Container(
-      color: Colors.blue,
+      color: Colors.orange,
       child: Text(
-        'Muhriddin\n' * 12,
+        '\n\n\n\n\nMuhriddin',
         textAlign: TextAlign.center,
         textDirection: TextDirection.ltr,
         style: TextStyle(color: Colors.black, fontSize: 60, fontStyle: FontStyle.normal),
